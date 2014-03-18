402abffc8b0d6d71794ec15ca65cab57cea8fedf
Luca <ldanzelmo@gmail.com>
2014-03-18 10:17:33 +0100

    Remember reserved RedBean Fused Methods in the model comments

--- app/models/Model_Guest.php
+++ app/models/Model_Guest.php
@@ -1,17 +1,26 @@
 <?php
 
 /**
- * Object model mapping for relational table `guest` 
+ * Object model mapping for relational table `guest`
+ *
+ * RedBean Fused methods
+ * R::store	$model->update()
+ * R::store	$model->after_update()
+ * R::load	$model->open()
+ * R::trash	$model->delete()
+ * R::trash	$model->after_delete()
+ * R::dispense	$model->dispense()
  */
+
 class Model_Guest extends RedBean_SimpleModel {
-    
+
     // lifecycle hooks
-    
+
     public function dispense() {
         $this->role = 'guest';
     }
-   
-    
+
+
     public function update() {
         $this->modify_date = date("Y-m-d H:i:s");
     }
